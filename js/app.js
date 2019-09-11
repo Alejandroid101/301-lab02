77a58ef1fe5058bf9c313daf0d78fbb87f832d39
ENRIQUE RIVERA <KIK.RDL3@GMAIL.COM>
2019-09-11 13:22:52 -0700

    feature 2 added, able to see keyword options now

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(function(){
   const horns = [];
+  const keywords = [];
 
   function Animal(data) {
     this.description = data.description;
@@ -9,6 +10,10 @@ $(function(){
     this.title = data.title;
 
     horns.push(this);
+
+    if (!keywords.includes(data.keyword)) {
+      keywords.push(data.keyword);
+    }
   }
 
   $.get('/data/page-1.json', (data) => {
@@ -22,6 +27,10 @@ $(function(){
       $(`#${hornId}`).append(`<h2>${horn.title}</h2>`);
       $(`#${hornId}`).append(`<img src="${horn.image}" alt="${horn.description}" width="250px" height="250px">`);
       $(`#${hornId}`).append(`<p>${horn.description}</p>`);
+    });
+
+    keywords.forEach(keyword => {
+      $('select').append(`<option value="${keyword}">${keyword}</option>`)
     });
   });
 });
