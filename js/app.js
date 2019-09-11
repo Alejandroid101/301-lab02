df70f1b6a1c3bfc8fca43c326c924be8f883ef5b
Ethan Davis <davisethan93@gmail.com>
2019-09-11 10:31:08 -0700

    Bullet 2 of Feature 1 done

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 $(function(){
-    $.get('/data/page-1.json', (data) => {
-        console.log(data);
+  const horns = [];
+
+  function Animal(data) {
+    this.description = data.description;
+    this.horns = data.horns;
+    this.image = data.image_url;
+    this.keyword = data.keyword;
+    this.title = data.title;
+
+    horns.push(this);
+  }
+
+  $.get('/data/page-1.json', (data) => {
+    // console.log(data);
+    data.forEach((el) => {
+      new Animal(el);
     });
-    
 
-
+    console.log(horns);
+  });
 });
